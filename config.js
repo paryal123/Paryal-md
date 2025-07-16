5bfa3b05e328328960bbc3f0d8f2da6eafc0401f
ArslanMD Official <numberkliye@gmail.com>
2025-07-16 17:40:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  // ADD YOUR SESSION ID 
+SESSION_ID: process.env.SESSION_ID || "ARSL~3942mCgZ#8QRvTVMRlRRHixuL21qsSb_08B5XtipNJ8mkGCm0iNM",  // ADD YOUR SESSION ID 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/atby2t.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that paryal-md is ALIVE now—ready to dive into new adventures with you!",
 SUDO_NB: process.env.SUDO_NB || "923467879144",
